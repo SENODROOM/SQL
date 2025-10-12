2012a9c0704fb2cef406fa39153943fab8417aa8
Muhammad Saad Amin <saadamin691@gmail.com>
2025-10-12 05:24:53 +0500

    2

--- Practise Problems/Foreign Keys.sql
+++ Practise Problems/Foreign Keys.sql
@@ -1,19 +1,19 @@
-CREATE TABLE Orders(
- userID INT PRIMARY KEY,
- FOREIGN KEY (userID) references Accounts (userID),
- userName VARCHAR(100),
- userEmail VARCHAR(100) UNIQUE,
- userAddress VARCHAR(255),
- userPhone VARCHAR(20),
- userPassword VARCHAR(100),
- userJoinDate VARCHAR(50),
- FOREIGN KEY (userName) references Accounts (userName),
- FOREIGN KEY (userEmail) references Accounts (userEmail),
- FOREIGN KEY (userAddress) references Accounts (userAddress),
- FOREIGN KEY (userPhone) references Accounts (userPhone),
- FOREIGN KEY (userPassword) references Accounts (userPassword),
- FOREIGN KEY (userJoinDate) references Accounts (userJoinDate)
+USE MDPharmacy;
+CREATE TABLE IF NOT EXISTS Orders(
+  orderID INT auto_increment PRIMARY KEY,
+  userID INT,
+  Orders VARCHAR(1000),
+  FOREIGN KEY (userID) REFERENCES Accounts(userID)
 );
+
+
+
+INSERT INTO Orders(Orders)
+VALUES (
+ "PANADOL"
+);
+
+
 
 DROP TABLE IF EXISTS Orders;
 
